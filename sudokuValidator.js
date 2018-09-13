31a3cbbbed06e505ae680543535420528781b429
Zumukich <212448545@logon.ds.ge.com>
2018-09-13 17:11:51 +0200

    validator refactor

--- sudokuValidator.js
+++ sudokuValidator.js
@@ -1,5 +1,114 @@
+function getRowItems(board, row) {
+	return board[row];
+}
+
+function getColItems(board, col) {
+	var retArray = [];
+	for (var i = 0; i < board.length; i++) {
+		retArray.push(board[i][col]);
+	}
+	return retArray;
+}
+
+function getGridItems(board, col, row) {
+	const gridSize = Math.sqrt(board.length);
+	var retArray = [];
+	startX = Math.floor(col / gridSize) * gridSize;
+	startY = Math.floor(row / gridSize) * gridSize;
+	for (var j = startY; j < startY + gridSize; j++) {
+		for (var i = startX; i < startX + gridSize; i++) {
+			retArray.push(board[i][j]);
+		}
+	}
+	return retArray;
+}
+
+function checkZeroes(array) {
+	return !array ? false : array.includes(0);
+}
+
+function checkDuplicates(array) {
+	return !array ? false : array.some((val, idx) => array.indexOf(val) !== idx);
+}
+
+console.log("zeroesF", checkZeroes([1, 2, 3, 4, 5, 6, 7, 8, "a", false, 9]));
+console.log("zeroesT", checkZeroes([1, 2, 3, 4, 5, 6, 7, 8, "a", false, 0, 9, 0]));
+console.log("zeroesF", checkZeroes([]));
+console.log("zeroesF", checkZeroes(undefined));
+console.log("zeroesF", checkZeroes(NaN));
+
+console.log("dupsF", checkDuplicates([1, 2, 3, 4, 5, 6, 7, 8, "a", false, 9]));
+console.log("dupsT", checkDuplicates([1, 2, 3, 4, 5, 6, 7, 8, "a", false, 0, 9, 0]));
+console.log("dupsF", checkDuplicates([]));
+console.log("dupsF", checkDuplicates(undefined));
+console.log("dupsF", checkDuplicates(NaN));
+
+function checkCompleteness(array, boardSize) {
+	var allPossibleNumbers = [];
+	for (var i = 0; i < boardSize; i++) allPossibleNumbers.push(i + 1);
+	var diffArr = allPossibleNumbers.filter(entry => !array.includes(entry));
+	console.log(typeof diffArr, diffArr, typeof [], [], diffArr == [], diffArr === []);
+	return diffArr == [];
+}
+
+console.log("completeT", checkCompleteness([3, 1, 2, 4], 4));
+console.log("completeF", checkCompleteness([1, 3, 4], 4));
+console.log("completeF", checkCompleteness([1, 10, 3, 4], 4));
+console.log("completeT", checkCompleteness([4, 5, 6, 7, 8, 9, 1, 2, 3], 9));
+console.log("completeF", checkCompleteness([4, 5, 5, 7, 8, 9, 1, 2, 3], 9));
+console.log("completeT", checkCompleteness([4, 5, 6, 7, 8, 9, 1, 2, 3, 10], 10));
+
+function getPossibleNumbers(board, col, row) {
+	var allPossibleNumbers = [];
+	for (var i = 0; i < board.length; i++) allPossibleNumbers.push(i + 1);
+	var usedNumbers = getRowItems(board, row)
+		.concat(getColItems(board, col)
+			.concat(getGridItems(board, row, col)))
+		.filter(element => element != 0);
+	return;
+}
+
+function duplicateExists(array) {
+	var tempWoZeros = array.filter(element => element !== 0);
+	return tempWoZeros.some((val, i) => tempWoZeros.indexOf(val) !== i);
+}
+
+function validateBoard(board) {
+	var valid = true;
+	for (var i = 0; i < board.length; i++)
+		if (duplicateExists(getRowItems(board, i)) || duplicateExists(getColItems(board, i))) valid = false;
+	return valid;
+}
+
 function sudokuValidator(board) {
 	var valid = true;
+	var allPossibleNumbers = [];
+	for (var i = 0; i < board.length; i++) allPossibleNumbers.push(i + 1);
+
+	for (var i = 0; i < board.length; i++) {
+		var checkedRow = getRowItems(board, i);
+		if (
+			checkedRow.indexOf(0) === -1 ||
+			checkedRow.some((val, i) => checkedRow.indexOf(val) !== i)
+		) valid = false;
+
+
+		var checkedCol = getColItems(board, i);
+		if (
+			checkedCol.indexOf(0) === -1 ||
+			checkedCol.some((val, i) => checkedRow.indexOf(val) !== i)
+		) valid = false;
+	}
+	return valid;
+
+	var usedNumbers = getRowItems(board, row)
+		.concat(getColItems(board, col)
+			.concat(getGridItems(board, row, col)))
+		.filter(element => element != 0);
+	return allPossibleNumbers
+		.filter(entry => !usedNumbers.includes(entry));
+
+
 	// for (var i = 0; i < 9; i++) {
 	// 	// Validate whether all rows add up to 45
 	// 	if (board[i].reduce((a, b) => a + b) != 45) valid = false;
@@ -27,7 +136,8 @@ function sudokuValidator(board) {
 }
 
 // From: https://www.codewars.com/kata/sudoku-solution-validator/
-// 4 kyu
+// also https://www.codewars.com/kata/did-i-finish-my-sudoku/
+// 4 (5) kyu
 
 console.log(sudokuValidator(
 	[

33cc492acd0825b012fa082841f51a6d98b93f26
Zumukich <peter.mamuzsics@ge.com>
2018-09-19 16:31:17 +0200

    New 6 kyu kata

---
+++ 6kyu/createPhoneNumber.js
@@ -0,0 +1,5 @@
+function createPhoneNumber(numbers) {
+	return numbers.join("").replace(/([0-9]{3})([0-9]{3})([0-9]{4})/, "($1) $2-$3");
+}
+
+console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]));

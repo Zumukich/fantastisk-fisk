b06f21ce492abe7e5cd6223cac9a11888df5f87b
Zumukich <peter.mamuzsics@ge.com>
2018-09-19 16:06:48 +0200

    Finished old 5 kyu kata

--- 5kyu/directionReduction.js
+++ 5kyu/directionReduction.js
@@ -1,23 +1,28 @@
 function dirReduc(directions) {
-
-    function opposite(cardinal) {
-        switch (cardinal) {
-            case "NORTH": return "SOUTH"; break;
-            case "SOUTH": return "NORTH"; break;
-            case "EAST": return "WEST"; break;
-            case "WEST": return "EAST"; break;
-            default: return "";
-		}
-    }
-
-    for (var i = 0; i < directions.length; i++) {
-        if (directions.indexOf(opposite(directions[i])) != -1) {
-
-		}
+	var opposite = {
+		"NORTH": "SOUTH",
+		"SOUTH": "NORTH",
+		"EAST": "WEST",
+		"WEST": "EAST"
 	}
+
+	if ((directions || []).length === 0 || !directions) return [];
+	for (var i = 0; i < directions.length - 1; i++) {
+		if (directions[i] == opposite[directions[i + 1]]) {
+			directions.splice(i, 2);
+			i -= 2;
+		}
+		// console.log(directions);
+	}
+	return directions;
 }
 
-console.log(dirReduc(["NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST"]));
+// From: https://www.codewars.com/kata/directions-reduction/
+// 5 kyu
 
-// console.log(dirReduc(["NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST"]) == ["WEST"]);
-// console.log(dirReduc(["NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH"]) == []);
+console.log(dirReduc(['EAST', 'EAST', 'WEST', 'NORTH', 'WEST', 'EAST', 'EAST', 'SOUTH', 'NORTH', 'WEST']));  // ['EAST', 'NORTH']
+console.log(dirReduc(["NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST"]));  // ['WEST']
+console.log(dirReduc(["NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH"]));          // []
+console.log(dirReduc([]));          // []
+console.log(dirReduc(0));          // []
+console.log(dirReduc(undefined));          // []

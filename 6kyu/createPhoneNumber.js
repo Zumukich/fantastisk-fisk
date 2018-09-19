f37caf49f8868454f34952bda9d7e58188f70444
Zumukich <peter.mamuzsics@ge.com>
2018-09-19 16:33:22 +0200

    Added link/kyu

--- 6kyu/createPhoneNumber.js
+++ 6kyu/createPhoneNumber.js
@@ -2,4 +2,7 @@ function createPhoneNumber(numbers) {
 	return numbers.join("").replace(/([0-9]{3})([0-9]{3})([0-9]{4})/, "($1) $2-$3");
 }
 
+// From: https://www.codewars.com/kata/create-phone-number/
+// 6 kyu
+
 console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]));

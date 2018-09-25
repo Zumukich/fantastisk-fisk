5d16f3986b43f54c2869ac1153db9c801f0238a4
Zumukich <peter.mamuzsics@ge.com>
2018-09-25 18:08:50 +0200

    Struggling

--- 4kyu/recoverSecret.js
+++ 4kyu/recoverSecret.js
@@ -3,30 +3,41 @@ function recoverSecret(triplets) {
 		.reduce((acc, val) => acc.concat(val), [])
 		.filter((val, idx, arr) => arr.indexOf(val) === idx)
 		.length;
-	var safetySwitch = 0;
 	do {
+		var seenTriplets = new Array(triplets.length).fill(0);
 		var secret = [];
 		triplets.push(triplets.shift());
 		for (var i = 0; i < 3; i++) secret.push(triplets[0][i]);
+		seenTriplets[0] = 1;
 		console.log(`Used ${secret} as a start`);
-		safetySwitch++;
+		var iterator = 1;
 
-		for (var iterator = 1; iterator < triplets.length; iterator++) {
+		do {
+			var usedHint = false;
 			if (triplets[iterator][1] === secret[0]) {
 				secret.unshift(triplets[iterator][0]);
 				console.log(`Used ${triplets[iterator]} to get to ${secret}`);
+				seenTriplets[iterator] = 1;
+				usedHint = true;
 			}
 			if (triplets[iterator][1] === secret[secret.length - 1]) {
 				secret.push(triplets[iterator][2]);
 				console.log(`Used ${triplets[iterator]} to get to ${secret}`);
+				seenTriplets[iterator] = 1;
+				usedHint = true;
 			}
 			if (secret.indexOf(triplets[iterator][2]) - secret.indexOf(triplets[iterator][0]) === 1) {
 				secret.splice(secret.indexOf(triplets[iterator][0]) + 1, 0, triplets[iterator][1]);
 				console.log(`Used ${triplets[iterator]} to get to ${secret}`);
+				seenTriplets[iterator] = 1;
+				usedHint = true;
 			}
-		}
+			iterator = usedHint ? seenTriplets.indexOf(0) : seenTriplets.indexOf(1) + 1;
+
+		} while (!seenTriplets.includes(0) || secret.length < messageLength)
+
 		console.log("--");
-	} while (secret.length < messageLength && safetySwitch < triplets.length);
+	} while (secret.length < messageLength);
 	return secret.join("");
 }
 
@@ -34,13 +45,13 @@ function recoverSecret(triplets) {
 // 4 kyu
 
 console.log(recoverSecret([
-	['a','t','s'],
-	['h','a','p'],
-	['t','i','s'],
-	['w','h','i'],
-	['t','s','u'],
-	['t','u','p'],
-	['w','h','s']
+	['a', 't', 's'],
+	['h', 'a', 'p'],
+	['t', 'i', 's'],
+	['w', 'h', 'i'],
+	['t', 's', 'u'],
+	['t', 'u', 'p'],
+	['w', 'h', 's']
 ]));
 
 console.log(recoverSecret([

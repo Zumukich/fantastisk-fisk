40a5f13063f55f115bb84cf4c6d0b006590d18fa
Zumukich <Zumu@anduril.zumu.hu>
2018-09-26 09:06:07 +0200

    More struggles

--- 4kyu/recoverSecret.js
+++ 4kyu/recoverSecret.js
@@ -11,31 +11,39 @@ function recoverSecret(triplets) {
 		seenTriplets[0] = 1;
 		console.log(`Used ${secret} as a start`);
 		var iterator = 1;
-
 		do {
 			var usedHint = false;
+			console.log(`Considering ${triplets[iterator]} for ${secret}`)
+			if (triplets[iterator][2] === secret[0]) {
+				secret.unshift(triplets[iterator][1]);
+				secret.unshift(triplets[iterator][0]);
+				console.log(`Used ${triplets[iterator]} to get to ${secret}`);
+				usedHint = true;
+			}
+			if (triplets[iterator][0] === secret[secret.length - 1]) {
+				secret.push(triplets[iterator][1]);
+				secret.push(triplets[iterator][2]);
+				console.log(`Used ${triplets[iterator]} to get to ${secret}`);
+				usedHint = true;
+			}
 			if (triplets[iterator][1] === secret[0]) {
 				secret.unshift(triplets[iterator][0]);
 				console.log(`Used ${triplets[iterator]} to get to ${secret}`);
-				seenTriplets[iterator] = 1;
 				usedHint = true;
 			}
 			if (triplets[iterator][1] === secret[secret.length - 1]) {
 				secret.push(triplets[iterator][2]);
 				console.log(`Used ${triplets[iterator]} to get to ${secret}`);
-				seenTriplets[iterator] = 1;
 				usedHint = true;
 			}
 			if (secret.indexOf(triplets[iterator][2]) - secret.indexOf(triplets[iterator][0]) === 1) {
 				secret.splice(secret.indexOf(triplets[iterator][0]) + 1, 0, triplets[iterator][1]);
 				console.log(`Used ${triplets[iterator]} to get to ${secret}`);
-				seenTriplets[iterator] = 1;
 				usedHint = true;
 			}
-			iterator = usedHint ? seenTriplets.indexOf(0) : seenTriplets.indexOf(1) + 1;
-
-		} while (!seenTriplets.includes(0) || secret.length < messageLength)
-
+			if (usedHint) seenTriplets[iterator] = 1;
+			iterator = usedHint ? seenTriplets.indexOf(0) : Math.max(iterator, seenTriplets.lastIndexOf(1)) + 1;
+		} while (iterator < triplets.length)
 		console.log("--");
 	} while (secret.length < messageLength);
 	return secret.join("");
@@ -44,15 +52,15 @@ function recoverSecret(triplets) {
 // From: https://www.codewars.com/kata/recover-a-secret-string-from-random-triplets/
 // 4 kyu
 
-console.log(recoverSecret([
-	['a', 't', 's'],
-	['h', 'a', 'p'],
-	['t', 'i', 's'],
-	['w', 'h', 'i'],
-	['t', 's', 'u'],
-	['t', 'u', 'p'],
-	['w', 'h', 's']
-]));
+// console.log(recoverSecret([
+// 	['a', 't', 's'],
+// 	['h', 'a', 'p'],
+// 	['t', 'i', 's'],
+// 	['w', 'h', 'i'],
+// 	['t', 's', 'u'],
+// 	['t', 'u', 'p'],
+// 	['w', 'h', 's']
+// ]));
 
 console.log(recoverSecret([
 	['t', 's', 'f'],

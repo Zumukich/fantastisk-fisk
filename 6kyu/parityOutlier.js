55d119f0a83e609dd48450a4484be27c98a871cd
Zumukich <Zumu@anduril.zumu.hu>
2018-10-26 01:19:47 +0200

    new kata and some housekeeping

--- 6kyu/parityOutlier.js
+++ 6kyu/parityOutlier.js
@@ -1,3 +1,23 @@
+function oldFindOdd(A) {
+    A.sort((a, b) => { return a - b });
+    while (A.length > 0) {
+        var counter = 1;
+        var examVal = A.shift();
+        while (A[0] == examVal) {
+            counter++;
+            A.shift();
+        }
+        if (counter % 2 == 1) {
+            return examVal;
+        }
+    }
+    return 0;
+}
+
+function findOdd(A) {
+    return A.sort((a, b) => a - b).reduce((acc, curVal, curIdx, arr) => acc == Infinity && curVal != arr[curIdx + 1] && curIdx % 2 == 0 ? curVal : acc, Infinity);
+}
+
 function findOutlier(integers) {
 	// Filter the original array to its even and odd elements
 	const even = integers.filter((num) => num % 2 == 0);
@@ -13,10 +33,19 @@ function iqTest(numbers) {
 	return (even.length < odd.length ? numArr.indexOf(even[0]) : numArr.indexOf(odd[0])) + 1;
 }
 
-// From: https://www.codewars.com/kata/find-the-parity-outlier/
-// also https://www.codewars.com/kata/iq-test
-// 6 kyu
+function stray(numbers) {
+	return numbers.sort()[0] === numbers.sort()[1] ? numbers[numbers.length - 1] : numbers[0];
+}
 
+// From: https://www.codewars.com/kata/find-the-odd-int/
+// also https://www.codewars.com/kata/find-the-parity-outlier/
+// also https://www.codewars.com/kata/iq-test
+// also https://www.codewars.com/kata/find-the-stray-number/
+// 6 kyu, 7 kyu
+
+console.log(stray([1, 1, 2]));
+console.log(findOdd([1, 1, 1, 1, 1, 1, 10, 1, 1, 1, 1]));
+console.log(oldFindOdd([1, 1, 2, -2, 5, 2, 4, 4, -1, -2, 5]));
 console.log(findOutlier([2, 4, 0, 100, 4, 11, 2602, 36]));
 console.log(findOutlier([160, 3, 1719, 19, 11, 13, -21]));
 console.log(findOutlier([113448820, -63909159, 150530110, -195538114, -125125936, 118231076, -183685662, 107700136, -59110720, -61981478, -73132362, -72277482, -110030498, -121323816, -137747948, 92340792, -11760466, 89916038, -35720926, 137455042, 783006, 85959956, 19673166, 41623264, -11566432, -43086886, 18807776, -80796962 ]));

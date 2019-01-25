4eb7fdb1819be138703f4dbd47bec844f34a6d9a
Zumukich <peter.mamuzsics@ge.com>
2019-01-25 13:59:24 +0100

    4 kyu baby, yeah!

--- 4kyu/decompose.js
+++ 4kyu/decompose.js
@@ -1,22 +1,36 @@
-function decompose(remainder, factors) {
-	if (remainder === 0) return true;
-	if (factors.length >= 2 && factors[factors.length - 1] === factors[factors.length - 2]) return false;
+function iterate(remainder, factors) {
+	if (remainder === 0) return factors;
 	for (let i = Math.floor(Math.sqrt(remainder)); i > 0; i--) {
-		if ((factors.length === 0 && Math.pow(i, 2) < remainder) ||	(factors.length > 0 && Math.pow(i, 2) <= remainder)) {
-			factors.push(i);
-			console.log(i, factors, factors[factors.length - 1]);
-			//  i < factors[factors.length - 1]
-			if (decompose(remainder - Math.pow(i, 2), factors)) {
-				return true;
-			} else {
-				factors.pop();
-				return false;
-			}
+		if ((factors.length === 0 && Math.pow(i, 2) < remainder) || (factors.length > 0 && Math.pow(i, 2) <= remainder && i < factors[0])) {
+			factors.unshift(i);
+			if (iterate(remainder - Math.pow(i, 2), factors))
+				return factors;
+			else
+				factors.shift();  // backtrack
 		}
 	}
+	return false;
 }
 
-var decomp = [];
+function decompose(n) {
+	var result = iterate(Math.pow(n, 2), []);
+	return result ? result : null;
+}
 
-decompose(144, decomp);
-console.log(decomp);
+// From: https://www.codewars.com/kata/square-into-squares-protect-trees
+// 4 kyu
+
+console.log(decompose(50));
+console.log(decompose(12));
+console.log(decompose(44));
+console.log(decompose(625));
+console.log(decompose(5));
+console.log(decompose(7100));
+console.log(decompose(123456));
+console.log(decompose(1234567));
+console.log(decompose(7654321));
+console.log(decompose(7654322));
+console.log(decompose(76));
+console.log(decompose(2));
+console.log(decompose(7));
+console.log(decompose(9927447));

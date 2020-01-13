f3d83dd2285541702c2edc4b477fcc26cd277404
Zumukich <peter.mamuzsics@ge.com>
2020-01-13 17:36:04 +0100

    Some practice

--- 6kyu/socialistRedistribution.js
+++ 6kyu/socialistRedistribution.js
@@ -1,15 +1,38 @@
 function socialistRedistribution(population, minimum) {
     if (population.reduce((a, b) => { return a + b }) < population.length * minimum) { return []; }
-    while ( Math.min(...population) < minimum ) {
+    while (Math.min(...population) < minimum) {
         population[population.indexOf(Math.max(...population))] -= 1;
         population[population.indexOf(Math.min(...population))] += 1;
     }
     return population;
 }
 
+function twoMax(arr) {
+    let times = 0;
+    console.log("Input", arr);
+    while (arr.indexOf(0) === arr.lastIndexOf(0)) {
+        arr.sort((a, b) => b - a);
+        let step = Math.max(1, Math.min(Math.floor(arr[0] / 2), arr[2]));
+        arr[1] -= step;
+        arr[0] -= step;
+        times += step;
+    }
+    return times;
+}
+
 // From: https://www.codewars.com/kata/socialist-distribution/
+// also: https://www.codewars.com/kata/5e0b72d2d772160011133654
 // 6 kyu
 
 console.log(socialistRedistribution([2, 1, 8, 10], 5));
 console.log(socialistRedistribution([2, 3, 5, 45, 45], 5));
 console.log(socialistRedistribution([32, 29, 50, 28, 10, 30, 4], -7));
+
+// console.log(twoMax([0, 0, 1]));
+// console.log(twoMax([2, 8, 0]));
+// console.log(twoMax([1, 2, 1]));
+// console.log(twoMax([7, 4, 10]));
+// console.log(twoMax([8, 2, 8]));
+console.log(twoMax([613232, 709387, 53095]));
+// console.log(twoMax([973815, 331601, 748625]));
+// console.log(twoMax([12, 12, 12]));
--- 7kyu/sumLowest.js
+++ 7kyu/sumLowest.js
@@ -2,8 +2,21 @@ function sumTwoLowest(arr) {
 	return parseInt(arr.splice(arr.indexOf(Math.min(...arr)), 1)) + Math.min(...arr);
 }
 
+function minSum(arr) {
+	let sum = 0;
+	do {
+		sum += parseInt(arr.splice(arr.indexOf(Math.min(...arr)), 1)) * parseInt(arr.splice(arr.indexOf(Math.max(...arr)), 1));
+	} while (arr.length > 0);
+	return sum;
+}
+
 // From: https://www.codewars.com/kata/sum-of-two-lowest-positive-integers
+// also: https://www.codewars.com/kata/5a523566b3bfa84c2e00010b
 // 7 kyu
 
-console.log(sumTwoLowest([19, 5, 42, 2, 77]));
-console.log(sumTwoLowest([10, 343445353, 3453445, 3453545353453]));
+//console.log(sumTwoLowest([19, 5, 42, 2, 77]));
+//console.log(sumTwoLowest([10, 343445353, 3453445, 3453545353453]));
+
+console.log(minSum([5, 4, 2, 3]));
+console.log(minSum([12, 6, 10, 26, 3, 24]));
+console.log(minSum([9, 2, 8, 7, 5, 4, 0, 6]));
